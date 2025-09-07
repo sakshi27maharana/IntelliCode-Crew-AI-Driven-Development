a8a7a4dedeb3ec0182200058e6356a9d0ae5376e
sakshi27maharana <60914206+sakshi27maharana@users.noreply.github.com>
2025-09-07 19:54:10 +0530

    add crew.py

---
+++ src/crew.py
@@ -0,0 +1,43 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+
+@CrewBase
+class Coder():
+    """Coder crew"""
+
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    # One click install for Docker Desktop:
+    #https://docs.docker.com/desktop/
+
+    @agent
+    def coder(self) -> Agent:
+        return Agent(
+            config=self.agents_config['coder'],
+            verbose=True,
+            allow_code_execution=True,
+            code_execution_mode="safe",  # Uses Docker for safety
+            max_execution_time=120, 
+            max_retry_limit=5
+    )
+
+    @task
+    def coding_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['coding_task'],
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Coder crew"""
+
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
